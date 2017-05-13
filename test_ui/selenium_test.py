7b19130c8754c3f089f7fa42e7c28a9862104271
Your Name <you@example.com>
2017-05-13 11:17:47 +0200

    test

---
+++ test_ui/selenium_test.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+import unittest
+
+class SimpleTest(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Chrome(executable_path='/home/tester/Pobrane/chromedriver')
+        self.driver.maximize_window()
+        self.driver.get('http://localhost:5000')
+
+    def test_content(self):
+        #WebDriverWait(self.driver, 5).until(EC.presence_of_element_located((By.XPATH, find_content)))
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
